fd7ffdcc8026030baea12e39572ff126f28c8bc1
Bilal Saeed <bilalsaeed232@gmail.com>
2018-06-03 15:43:06 +0200

    ADDED: Edit & Remove Buttons

--- src/Note.js
+++ src/Note.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import FaPencil from 'react-icons/lib/fa/pencil';
+import FaTrash from 'react-icons/lib/fa/trash';
+
 
 class Note extends Component {
     render(){
         return (
             <div class="note">
                 <p>Sample Note...</p>
+                <span>
+                    <a id="edit" class="btn"><FaPencil /></a>
+                    <a id="remove" class="btn"><FaTrash /></a>
+                </span>
             </div>
         )
     }
